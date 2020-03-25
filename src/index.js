41753a34ab09b7a4136839f1e17756d03ac635f8
Michael Skeen <mmskeen@gmail.com>
2020-03-25 12:58:15 -0600

    Applied style based on time of day

--- src/index.js
+++ src/index.js
@@ -5,3 +5,27 @@
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
+
+import React from "react";
+import ReactDom from "react-dom";
+
+const date = new Date(2020, 3, 25, 10);
+const timeOfDay =
+  date.getHours() < 12
+    ? "Morning"
+    : date.getHours() < 18
+    ? "Afternoon"
+    : "Evening";
+
+const style = { color: "" };
+style.color =
+  date.getHours() < 12 ? "red" : date.getHours() < 18 ? "green" : "blue";
+
+ReactDom.render(
+  <div>
+    <h1 className="heading" style={style}>
+      Good {timeOfDay}
+    </h1>
+  </div>,
+  document.getElementById("root")
+);
